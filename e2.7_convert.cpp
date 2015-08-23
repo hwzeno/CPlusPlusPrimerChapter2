9f8ec6c678f1f8ef4dff7ff5d735e9edcb2656d7
drzeno <drzeno@yeah.net>
2015-08-23 11:34:11 +0800

    Update e2.7_convert.cpp

--- e2.7_convert.cpp
+++ e2.7_convert.cpp
@@ -10,7 +10,7 @@ int main()
 	std::cout << "F\145rgus.\012";//e  \n
 
 	std::cout << 3.14e1L << std::endl;
-	//std::cout << 1024f << std::endl;//编译不通过，报错，整型后不能跟f
+	//std::cout << 1024f << std::endl;//编译不通过，报错，整型后不能跟f。
 	std::cout << 3.14L << std::endl;
 
 	return 0;
