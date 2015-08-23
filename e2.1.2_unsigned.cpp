8296e29bd5bf7d94b025afef161bf3fe12426079
drzeno <drzeno@yeah.net>
2015-08-23 10:06:29 +0800

    unsigned

--- e2.1.2_unsigned.cpp
+++ e2.1.2_unsigned.cpp
@@ -4,4 +4,18 @@
     > Mail:340562424@qq.com 
     > Created Time: 2015年08月22日 星期六 22时17分22秒
  ********************************/
+#include<iostream>
+int main()
+{
+	unsigned u = 10, u2 = 42;
+	std::cout << u2 - u << std::endl;
+	std::cout << u - u2 << std::endl;
+
+	int i = 10, i2 = 42;
+	std::cout << i2 - i << std::endl;
+	std::cout << i - i2 << std::endl;
+	std::cout << i - u << std::endl;
+	std::cout << u - i << std::endl;
+
+}
 
