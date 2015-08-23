666e869b8c1ad3bc02a4d6eab0070d7ce81df215
drzeno <drzeno@yeah.net>
2015-08-23 10:23:24 +0800

    literal

---
+++ e2.7_convert.cpp
@@ -0,0 +1,18 @@
+/*********************************
+    > File Name: e2.7_convert.cpp
+    > Author: Zeno
+    > Mail:340562424@qq.com 
+    > Created Time: 2015年08月23日 星期日 10时12分06秒
+ ********************************/
+#include<iostream>
+int main()
+{
+	std::cout << "F\145rgus.\012";//e  \n
+
+	std::cout << 3.14e1L << std::endl;
+	//std::cout << 1024f << std::endl;
+	std::cout << 3.14L << std::endl;
+
+	return 0;
+
+}
